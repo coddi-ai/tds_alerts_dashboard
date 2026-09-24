93ab1a3e1c10bae168b3efef694c1d9c4688d862
Patricio Ortiz <132593508+portizv26@users.noreply.github.com>
2026-09-24 15:31:12 -0300

    new version!

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ from src.data.catalog import dashboard_data_root
 
 # Dashboard release version, shown as a footnote on the login page.
 # Bump this manually whenever a new version is deployed.
-APP_VERSION = "2.1.23"
+APP_VERSION = "2.1.24"
 
 
 class Settings(BaseSettings):
